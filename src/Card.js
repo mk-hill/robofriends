b8ce1c6ceb96bbb5abb9eeb2eb509c461fc8f67d
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-14 19:00:15 -0500

    Add robot cards

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Destructuring props
+const Card = ({ id, name, email }) => {
+  return (
+    <div className="bg-lightest-blue dib br3 pa3 ma2 grow be shadow-5 tc">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
+      <div>
+        <h1>{name}</h1>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Legion',
+    username: '123e4567-e89b',
+    email: '123e4567@gethr.us',
+  },
+  {
+    id: 2,
+    name: 'C-3PO',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+  },
+  {
+    id: 3,
+    name: 'Liberty Prime',
+    username: 'Democracy_Non_Negotiable',
+    email: 'lib_prime@army.mil',
+  },
+  {
+    id: 4,
+    name: 'Bender',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+  },
+  {
+    id: 5,
+    name: 'Data',
+    username: 'Lore',
+    email: 'Lucio_Hettinger@annie.ca',
+  },
+  {
+    id: 6,
+    name: 'GLaDOS',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+  },
+  {
+    id: 7,
+    name: 'Centurion',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz',
+  },
+  {
+    id: 8,
+    name: 'Servitor',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me',
+  },
+  {
+    id: 9,
+    name: 'HAL 9000',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io',
+  },
+  {
+    id: 10,
+    name: 'HK-47',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz',
+  },
+];
