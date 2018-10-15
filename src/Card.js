4507a0d8f277d1dc7d65d80d0b019083cb61e927
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-15 18:12:04 -0500

    Update styling

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ const Card = ({ id, name, email }) => {
     <div className="bg-light-green dib br3 pa3 ma2 grow be shadow-5 tc">
       <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
       <div>
-        <h1>{name}</h1>
+        <h2>{name}</h2>
         <p>{email}</p>
       </div>
     </div>
