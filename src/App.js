b4bd9845f52284b93aba681450568fee73f63a22
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-15 17:58:11 -0500

    Add search bar and title

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: '',
+    };
+  }
+
+  onSearchChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot =>
+      robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    );
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Destructuring props
 const Card = ({ id, name, email }) => {
   return (
-    <div className="bg-lightest-blue dib br3 pa3 ma2 grow be shadow-5 tc">
+    <div className="bg-light-green dib br3 pa3 ma2 grow be shadow-5 tc">
       <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
       <div>
         <h1>{name}</h1>
