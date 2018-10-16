7bc3b60dff2678041d96f958f7ee32a0aba7786c
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-15 19:59:15 -0500

    Updates

---
+++ precache-manifest.0e5ac2d7b6d4467cb99c319e229a3385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "86e2e71bd1d8c2f34ab1",
+    "url": "/robofriends/static/js/main.86e2e71b.chunk.js"
+  },
+  {
+    "revision": "3e7477c03b89c6218fde",
+    "url": "/robofriends/static/js/1.3e7477c0.chunk.js"
+  },
+  {
+    "revision": "86e2e71bd1d8c2f34ab1",
+    "url": "/robofriends/static/css/main.cb6ed97b.chunk.css"
+  },
+  {
+    "revision": "3e7477c03b89c6218fde",
+    "url": "/robofriends/static/css/1.eccdfd82.chunk.css"
+  },
+  {
+    "revision": "378e551a364319e2d0c2535112532666",
+    "url": "/robofriends/index.html"
+  }
+];
