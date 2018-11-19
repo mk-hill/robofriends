142403c42eff9475f39daa7ff7ecde52d55f7e89
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-11-19 12:29:53 -0600

    Implement redux-thunk for initial fetch

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,23 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './constants.js';
 
 export const setSearchField = text => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text,
 });
+
+// Using redux-thunk to invoke returned function with dispatch
+// Redux out of the box appears to expect actions to return objects only
+export const requestRobots = () => dispatch => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(usersData =>
+      dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: usersData })
+    )
+    .catch(err => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: err }));
+};
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,36 +4,45 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrrorBoundry from '../components/ErrorBoundry';
-import { robots } from '../robots';
+// import { robots } from '../robots';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField,
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    // onRequestRobots: () => requestRobots(dispatch),
+    onRequestRobots: () => dispatch(requestRobots()),
   };
 };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      // searchField: '',
-    };
-  }
+  // Constructor no longer required, robots received as props
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     // searchField: '',
+  //   };
+  // }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
+    // Earlier fetch call without redux:
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(res => res.json())
+    //   .then(users => this.setState({ robots: users }));
+    // Earlier local robots file without fetch:
     // this.setState({ robots: robots });
   }
 
@@ -43,12 +52,12 @@ class App extends Component {
 
   render() {
     // const { robots, searchField } = this.state;
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    // const { robots } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot =>
       robot.name.toLowerCase().includes(searchField.toLowerCase())
     );
-    return !robots.length ? (
+    return isPending ? (
       <h1 className="tc f1 mt5">Loading RoboFriends</h1>
     ) : (
       <div className="tc">
